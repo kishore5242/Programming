165112e3a41ce40cbbb8754e4e6fefd9354de060
kishore5242 <kishore5242@gmail.com>
2019-06-15 12:04:19 +0530

    New project for learning design patterns in java

---
+++ design-patterns/src/com/kishore5242/dp/singleton/Runner.java
@@ -0,0 +1,12 @@
+package com.kishore5242.dp.singleton;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// singleton design pattern explained
+		System.out.println("hello world!");
+	}
+
+}
